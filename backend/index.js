80f0d029e1b31f2860244865fb415d18a647810e
Valentin Bourdier <valentin.bourdier@mail.com>
2019-02-22 20:14:00 +0100

    Added some comments

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+// Includes
+
 var exec = require('child_process').exec;
 
 const express = require('express')
@@ -7,8 +9,12 @@ var uniqid = require('uniqid');
 
 const log = require('simple-node-logger').createSimpleLogger();
 
+// Some constants
+
 var INPUT_FILTER = "input{stdin{}}";
 var OUTPUT_FILTER = "output{stdout{}}";
+
+// Environments variables
 
 const PORT = process.env.PORT || 8081;
 const MAX_EXEC_TIMEOUT = process.env.MAX_EXEC_TIMEOUT || 60000;
@@ -16,17 +22,54 @@ const LOGSTASH_DATA_DIR = process.env.LOGSTASH_DATA_DIR || "/tmp/logstash/data/"
 const LOGSTASH_RAM = process.env.LOGSTASH_RAM || "1g";
 const LOG_LEVEL = process.env.LOG_LEVEL || "info";
 
+//////////////////
+//  Server part //
+//////////////////
+
+// Some init definitions
+
 log.setLevel(LOG_LEVEL);
 
 const app = express()
-
 app.use(express.json());
 app.use(cors())
+
+// Home rooting
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ "message": "Nothing here !" }));
 })
+
+// Rooting for process starting
+
+app.post('/start_process', function (req, res) {
+    
+    id = uniqid()
+
+    log.info(id + " - Start a process hit");
+
+    if (argumentsValids(req, res)) {
+        var input_data = quote([req.body.input_data]);
+
+        var logstash_filter = quote([INPUT_FILTER + req.body.logstash_filter + OUTPUT_FILTER]);
+
+
+        computeResult(id, res, input_data, logstash_filter);
+    }
+})
+
+// We start the server
+
+app.listen(PORT, function () {
+    log.info('App listening on port ' + PORT);
+})
+
+////////////////////////
+//  Compute functions //
+////////////////////////
+
+// Compute the logstash result
 
 function computeResult(id, res, input, filter) {
     log.info(id + " - Starting logstash process");
@@ -52,12 +95,13 @@ function computeResult(id, res, input, filter) {
             status: status
         };
 
-
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({ "succeed": true, "job_result": job_result}));
     });
 
 }
+
+// Fail because of bad parameters
 
 function failBadParameters(id, res, missing_fields) {
     log.warn(id + " - Bad parameters for request");
@@ -66,6 +110,8 @@ function failBadParameters(id, res, missing_fields) {
     res.status(400);
     res.send(JSON.stringify({ "succeed": false, "missing_fields": missing_fields }));
 }
+
+// Check if provided arguments are valids
 
 function argumentsValids(req, res) {
     var ok = true
@@ -88,23 +134,3 @@ function argumentsValids(req, res) {
 
     return ok
 }
-
-app.post('/start_process', function (req, res) {
-    
-    id = uniqid()
-
-    log.info(id + " - Start a process hit");
-
-    if (argumentsValids(req, res)) {
-        var input_data = quote([req.body.input_data]);
-
-        var logstash_filter = quote([INPUT_FILTER + req.body.logstash_filter + OUTPUT_FILTER]);
-
-
-        computeResult(id, res, input_data, logstash_filter);
-    }
-})
-
-app.listen(PORT, function () {
-    log.info('App listening on port ' + PORT);
-})
