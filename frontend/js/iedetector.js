dc824163ad70425e019f2c3441a9f92ba28e7c37
Valentin Bourdier <valentin.bourdier@mail.com>
2019-02-24 17:03:41 +0100

    We check if browser is IE

---
+++ frontend/js/iedetector.js
@@ -0,0 +1,18 @@
+// Check if browser is Internet Explorer
+function isIE() {
+    var ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+    var msie = ua.indexOf('MSIE '); // IE 10 or older
+    var trident = ua.indexOf('Trident/'); //IE 11
+
+    return (msie > 0 || trident > 0);
+}
+
+
+//function to show alert if it's IE
+function ShowIEAlert(){
+    if(isIE()){
+       alert("Due to performance issues, Internet Explorer is really not recommended. Please use Firefox or Chrome with this website.");
+    }
+}
+
+ShowIEAlert()
