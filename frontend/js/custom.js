73bee7c29ffc671296a3c5047f7b0ec332adf0b1
Valentin Bourdier <valentin.bourdier@mail.com>
2019-02-24 13:44:22 +0100

    Save and load shortcut are now working for the editor

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -19,6 +19,56 @@ editor.setOptions({
   keyboardHandler: "ace/keyboard/sublime"
 })
 
+// Editor save and open behavior
+
+editor.commands.addCommand({
+  name: 'save',
+  bindKey: {win: "Ctrl-S", "mac": "Cmd-S"},
+  exec: function(editor) {
+      data = editor.session.getValue()
+      saveToFile(data, "logstash_filter.conf")
+  }
+})
+
+editor.commands.addCommand({
+  name: 'open',
+  bindKey: {win: "Ctrl-O", "mac": "Cmd-O"},
+  exec: function(editor) {
+      $('#filter_input_loading').click();
+  }
+})
+
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    editor.session.setValue(contents);
+  };
+  reader.readAsText(file);
+}
+
+document.getElementById('filter_input_loading')
+  .addEventListener('change', readSingleFile, false);
+
+
+// Util functions
+
+function saveToFile(data, filename) {
+  var blob = new Blob([data], {type: 'text/plain'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/plain', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+}
+
 // Theme color
 
 function enableBlackTheme() {
