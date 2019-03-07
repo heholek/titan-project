2fc27b9e567da836460af7c79feda80907741054
Valentin Bourdier <valentin.bourdier@mail.com>
2019-03-07 22:21:31 +0100

    Added a tiny script to update project from command-line

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -d ".git" ]; then
+  echo "We are not in a git local repository, I cannot continue"
+  exit 1
+fi
+
+echo "Getting latest version if any"
+
+cp docker-compose.yml docker-compose-old.yml
+git reset --hard
+git pull
+cp docker-compose.yml docker-compose-latest.yml
+cp docker-compose-old.yml docker-compose.yml
+
+read -p "Do you want to launch the build and deployment of the new version with docker ? " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker-compose up -d
+fi
