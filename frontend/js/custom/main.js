f0f8d2385d3aa6e05cd810ec1f7c44aa52a7eeab
Valentin Bourdier <valentin.bourdier@mpsa.com>
2019-10-30 16:22:55 +0100

    Added possibility to disable file upload

--- frontend/js/custom/main.js
+++ frontend/js/custom/main.js
@@ -1,5 +1,6 @@
 const api_url = "http://localhost:8081";
 const enable_news = true
+const enable_file_upload = true
 
 // Set default values
 
@@ -9,3 +10,7 @@ loadLogstashVersions(function () {
     loadSession()
     loadShareConfigIfNeeded()
 })
+
+if(enable_file_upload) {
+    $('#file_upload_feature').removeClass('d-none')
+}
