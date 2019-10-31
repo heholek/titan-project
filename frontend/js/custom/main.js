9225e8da04b88a6db2377c78caf7dc98ff692101
Valentin Bourdier <valentin.bourdier@mail.com>
2019-11-01 00:35:54 +0100

    Guess a file can now be disabled

--- frontend/js/custom/main.js
+++ frontend/js/custom/main.js
@@ -1,6 +1,7 @@
 const api_url = "http://localhost:8081";
 const enable_news = true
 const enable_file_upload = true
+const enable_guess_file = true
 
 // Set default values
 
@@ -13,4 +14,11 @@ loadLogstashVersions(function () {
 
 if(enable_file_upload) {
     $('#file_upload_feature').removeClass('d-none')
+}
+
+if(!enable_guess_file) {
+    $('#column_guess_file').addClass('d-none')
+    $('#column_empty_right').addClass('d-none')
+    $('#column_clear_form').addClass('col-lg-4')
+    $('#column_start_process').addClass('col-lg-4')
 }
