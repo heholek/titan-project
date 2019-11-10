c75200dd823ceffa3169e3773ace42f3ad802344
Valentin Bourdier <valentin.bourdier@mpsa.com>
2019-11-10 01:48:31 +0100

    Added one new testcase

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -79,6 +79,31 @@ describe("API Testing", function () {
         });
       });
 
+      it("should work with custom pattern and disabled grok pattern configuration", function (done) {
+        if (!enable_slow_tests) this.skip()
+
+        formData = {
+          input_data: "HAHA\nHOHO\nHUHU",
+          custom_logstash_patterns: "STR_UPPER [A-Z]*\n",
+          logstash_filter: "filter {\n  grok {\n    match => {\n      'message' => '%{STR_UPPER:test}'\n    }\n    patterns_dir => '/idontexists'\n    patterns_files_glob => '*.fake_extension'\n  }\n}",
+          input_extra_fields: [],
+          logstash_version: logstashVersion
+        }
+        request.post({ url: url, body: formData, json: true }, function (error, response, body) {
+          expect(body.config_ok).to.equal(true);
+          expect(body.succeed).to.equal(true);
+          expect(response.statusCode).to.equal(200);
+          expect(body.job_result.status).to.equal(0);
+          expect(body.job_result.stdout).to.match(/HAHA/);
+          expect(body.job_result.stdout).to.match(/HOHO/);
+          expect(body.job_result.stdout).to.match(/test/);
+
+          expect(body.job_result.stdout).not.to.match(/HUHU/);
+
+          done();
+        });
+      });
+
       it("should work with basic parameters", function (done) {
         if (!enable_slow_tests) this.skip()
 
